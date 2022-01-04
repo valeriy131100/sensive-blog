5b67fffd4b160e800ea16b4407445a54936b7f2d
Valery Efremov <valeriy131100@gmail.com>
2022-01-04 17:37:53 +0300

    Remove unused functions

--- blog/views.py
+++ blog/views.py
@@ -3,25 +3,7 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def get_related_posts_count(tag):
-    return tag.posts.count()
-
-
 def serialize_post(post):
-    return {
-        'title': post.title,
-        'teaser_text': post.text[:200],
-        'author': post.author.username,
-        'comments_amount': len(Comment.objects.filter(post=post)),
-        'image_url': post.image.url if post.image else None,
-        'published_at': post.published_at,
-        'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
-        'first_tag_title': post.tags.all()[0].title,
-    }
-
-
-def serialize_post_optimized(post):
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -30,19 +12,12 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.all()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
 
 def serialize_tag(tag):
-    return {
-        'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
-    }
-
-
-def serialize_tag_optimized(tag):
     return {
         'title': tag.title,
         'posts_with_tag': tag.posts_count,
@@ -71,13 +46,13 @@ def index(request):
 
     context = {
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
         'page_posts': [
-            serialize_post_optimized(post) for post in most_fresh_posts
+            serialize_post(post) for post in most_fresh_posts
         ],
         'popular_tags': [
-            serialize_tag_optimized(tag) for tag in most_popular_tags
+            serialize_tag(tag) for tag in most_popular_tags
         ],
     }
     return render(request, 'index.html', context)
@@ -108,7 +83,7 @@ def post_detail(request, slug):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag_optimized(tag) for tag in related_tags],
+        'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
     most_popular_tags = (Tag.objects.popular()
@@ -127,10 +102,10 @@ def post_detail(request, slug):
     context = {
         'post': serialized_post,
         'popular_tags': [
-            serialize_tag_optimized(tag) for tag in most_popular_tags
+            serialize_tag(tag) for tag in most_popular_tags
         ],
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, 'post-details.html', context)
@@ -160,13 +135,13 @@ def tag_filter(request, tag_title):
     context = {
         'tag': tag.title,
         'popular_tags': [
-            serialize_tag_optimized(tag) for tag in most_popular_tags
+            serialize_tag(tag) for tag in most_popular_tags
         ],
         'posts': [
-            serialize_post_optimized(post) for post in related_posts
+            serialize_post(post) for post in related_posts
         ],
         'most_popular_posts': [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, 'posts-list.html', context)
