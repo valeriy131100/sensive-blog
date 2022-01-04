e7b8bc21ca76878541181852d18b2b3e5ecb5c95
Valery Efremov <valeriy131100@gmail.com>
2022-01-04 15:16:21 +0300

    Reduce queries count with annotate posts count for tag

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,10 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [
+            serialize_tag_optimized(tag)
+            for tag in post.tags.annotate(posts_count=Count('posts'))
+        ],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -39,6 +42,13 @@ def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts_count,
     }
 
 
@@ -54,7 +64,8 @@ def index(request):
                                .prefetch_related('author'))
     most_fresh_posts = fresh_posts[:5]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = (Tag.objects.popular()
+                                    .annotate(posts_count=Count('posts'))[:5])
 
     context = {
         'most_popular_posts': [
@@ -63,7 +74,9 @@ def index(request):
         'page_posts': [
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
     }
     return render(request, 'index.html', context)
 
